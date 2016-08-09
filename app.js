3ed2104ad1e7a90e6278d3c3ef7a72b17f561c46
wcordero <me@waltercordero.com>
2016-08-09 10:53:04 -0600

    step-3

--- app.js
+++ app.js
@@ -1,53 +1,21 @@
+var router = require("./router");
 //Problema: Necesitamos una manera visual de mostrar el resultado obtenido de la consulta hacia la OMDBD API
 //Solucion: Utilizar Node.js to mostrar el resultado obtenido de la busqueda en un template via HTTP
 
-//1. Crear un Servidor Web
+//Crear un Servidor Web
 const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer(function (request, response){
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Primer Texto desde un Servidor Web con Node.js');
+  router.home(request, response);
+  router.result(request, response);
 })
 
 server.listen(port, hostname, function () {
   console.log('Servidor corriendo en http://' + hostname + ':'+ port);
 })
-
-//2. Manejar las rutas HTTP para GET y POST
-function homeRoute(request, response){
-  //Si la url == "/" && el verbo es GET
-  if(request.url === "/"){
-    //Mostramos search.html
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write("Header\n");
-    response.write("Search\n");
-    response.end("Footer\n");
-  }
-  //Si la url == "/" && el verbo es POST
-    //Redireccionamos hacia /:omdbSearch
-}
-
-//3. Manejar la peticion HTTP GET para la ruta /:omdbSearch eje. /Batman
-function resultRoute(request, response){
-  //Si la url == "/..."
-  var omdbSearch = request.url.replace("/", "");
-
-  if (omdbSearch.length > 0){
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write("Header\n");
-    response.write(omdbSearch + "\n");
-    response.end("Footer\n");
-
-    //Obtenemos el JSON del API de omdb
-      //al ejecutar el evento "end"
-        //Mostramos result.html
-      //al ejecutar el evento "error"
-        //Mostramos error.html
-  }
-}
 
 //4. Creacion de una funcion que permita el manejo de la lectura de archivos y mezclar los valores
   // que se leen y obtener un string
---
+++ router.js
@@ -0,0 +1,57 @@
+var Omdb = require("./omdb");
+
+//Manejar las rutas HTTP para GET y POST
+function home(request, response){
+  //Si la url == "/" && el verbo es GET
+  if(request.url === "/"){
+    //Mostramos search.html
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write("Header\n");
+    response.write("Search\n");
+    response.end("Footer\n");
+  }
+  //Si la url == "/" && el verbo es POST
+    //Redireccionamos hacia /:omdbSearch
+}
+
+//Manejar la peticion HTTP GET para la ruta /:omdbSearch eje. /Batman
+function result(request, response){
+  //Si la url == "/..."
+  var omdbSearch = request.url.replace("/", "");
+
+  if (omdbSearch.length > 0){
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write("Header\n");
+
+    //Obtenemos el JSON del API de omdb
+    var omdbResult = new omdb(omdbSearch);
+    //al ejecutar el evento "end"
+    omdbResult.on("end", function (resultJSON){
+      //Mostramos result.html
+
+      //Almacenamos el resultado en un Objeto Nuevo
+      var values = {
+        title: resultJSON.Title,
+        rated: resultJSON.Rated,
+        genre: resultJSON.Genre,
+        awards: resultJSON.Awards,
+        rating: resultJSON.imdbRating,
+        plot: resultJSON.Plot
+      }
+
+      response.write(values.title + ',' + values.plot + ".\n");
+      response.end("Footer\n");
+    });
+
+    //al ejecutar el evento "error"
+    omdbResult.on("error", function (error){
+        //Mostramos error.html
+        response.write(error.message + "\n");
+        response.end('Footer\n');
+    })
+
+  }
+}
+
+module.exports.home = home;
+module.exports.result = result;
